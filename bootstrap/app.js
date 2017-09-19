58f62cceec3f0908ed1949f4870655cb05d0b062
Lucas Rodrigues <lucas.sirodrigues@gmail.com>
2017-09-19 10:40:03 -0300

    Realizando o parse do body da requisição

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,9 +1,14 @@
+var bodyParser = require('body-parser')
 var consign = require('consign')
 var logger = require('morgan')
 var express = require('express')
 var app = express()
 
 app.use(logger('dev'))
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+app.use(bodyParser.json())
 require('../routes/index')(app)
 
 consign().include('routes').into(app)
