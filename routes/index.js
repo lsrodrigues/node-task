98934f15cd081378ed2cac07d161a4b261f55f37
Lucas Rodrigues <lucas.sirodrigues@gmail.com>
2017-09-19 15:55:59 -0300

    Mudando o path raiz do app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var tasks = require('../controllers/tasks')
 module.exports = function (app) {
-    app.get('/all', tasks.index)
+    app.get('/', tasks.index)
     app.post('/', tasks.store)
     app.put('/task/:id',  tasks.update)
     app.delete('/task/:id', tasks.destroy)
